183ca73ee7193e8bb7a8cd7ac3ff90c44a0aba8b
sunwin1029 <sunwin1029@gmail.com>
2025-08-05 18:07:32 +0900

    wef

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/LogIn';
 import Register from './components/Register';
-import TodoList from './components/ToDoList';
+import TodoList from './components/TodoList';
 import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
